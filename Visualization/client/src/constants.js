0e95d4e851908f87ead5a00271cd15089de01707
p.cityzen17 <p.cityzen17@gmail.com>
2023-10-31 22:21:22 -0500

    1st commit

---
+++ Visualization/client/src/constants.js
@@ -0,0 +1,25 @@
+
+
+// MAP CONSTANTS
+export const MAPBOX_TOKEN = "pk.eyJ1IjoicGNpdHl6ZW4xNzE3IiwiYSI6ImNsb2Y1MmFlYzBpNGEyaW1wODdhZXBsY2QifQ.HnV8jPLHdFCNMwBg6890aw";
+
+
+export const startingViewPort = {
+    theme: "theme_dark",
+    viewState: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: 42.3248500323765,
+        longitude: -71.08208410323299,
+        zoom: 11.45882051006856,
+        maxZoom: 20,
+        pitch: 45,
+        rotating: false,
+        bearing: 0
+    }
+};
+
+
+
+// ACTIONS
+export const CHANGE_VIEWPORT = "CHANGE_VIEWPORT";
